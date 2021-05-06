a112c476f331dbffd0720db2c792d0d3d238fde1
thxnk <58910440+thxnk@users.noreply.github.com>
2021-05-06 12:21:41 +0300

    Update CLIdemo.java

--- src/com/mybank/tui/CLIdemo.java
+++ src/com/mybank/tui/CLIdemo.java
@@ -61,46 +61,7 @@ public class CLIdemo {
                 printHelp();
             }
             else if("report".equals(line)){
-                System.out.println("\nCUSTOMERS REPORT");
-                System.out.println("================");
-
-                for(int cust_idx = 0; cust_idx < Bank.getNumberOfCustomers(); ++cust_idx) {
-                    Customer customer = Bank.getCustomer(cust_idx);
-                    System.out.println();
-                    System.out.println("Customer: " + customer.getLastName() + ", " + customer.getFirstName());
-
-                    for(int acct_idx = 0; acct_idx < customer.getNumberOfAccounts(); ++acct_idx) {
-                        Account account = customer.getAccount(acct_idx);
-                        String account_type = "";
-                        if (account instanceof SavingsAccount) {
-                            account_type = "Savings Account";
-                        } else if (account instanceof CheckingAccount) {
-                            account_type = "Checking Account";
-                        } else {
-                            account_type = "Unknown Account Type";
-                        }
-
-                        System.out.println("    " + account_type + ": current balance is " + account.getBalance());
-                    }
-                }
-            }
-            else if ("customers".equals(line)) {
-                AttributedStringBuilder a = new AttributedStringBuilder()
-                        .append("\nThis is all of your ")
-                        .append("customers", AttributedStyle.BOLD.foreground(AttributedStyle.RED))
-                        .append(":");
-
-                System.out.println(a.toAnsi());
-                if (Bank.getNumberOfCustomers() > 0) {
-                    System.out.println("\nLast name\tFirst Name\tBalance");
-                    System.out.println("---------------------------------------");
-                    for (int i = 0; i < Bank.getNumberOfCustomers(); i++) {
-                        System.out.println(Bank.getCustomer(i).getLastName() + "\t\t" + Bank.getCustomer(i).getFirstName() + "\t\t$" + Bank.getCustomer(i).getAccount(0).getBalance());
-                    }
-                } else {
-                    System.out.println(ANSI_RED+"Your bank has no customers!"+ANSI_RESET);
-                }
-
+                new CustomerReport().generateReport();
             } else if (line.indexOf("customer") != -1) {
                 try {
                     int custNo = 0;
